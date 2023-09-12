8874927ac3cf91c07e4770fe4463b79e0d5020bc
Raemond <raemondcarta@gmail.com>
2023-09-12 15:13:57 -0400

    End of section 2

---
+++ API/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+//creates database on start application instead of dependency injection from using services
+using var scope = app.Services.CreateScope(); // when werefinish with this particular method with this scope it would be destroyed and cleaned up from memory as soon as we execute this following code
+var services = scope.ServiceProvider;
+
+try
+{
+   var context = services.GetRequiredService<DataContext>(); 
+   await context.Database.MigrateAsync();
+   await Seed.SeedData(context);
+}
+catch (Exception ex)
+{
+    
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
+app.Run();
